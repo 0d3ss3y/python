6dc7a2236c5c3c7fcd37ace5dc94e924ecd4b284
Letlotlo Khoali <154567290+0d3ss3y@users.noreply.github.com>
2024-08-13 03:43:28 +0200

    Operational Complete

--- Data_Structures/Dictionaries/dic.py
+++ Data_Structures/Dictionaries/dic.py
@@ -1,1 +1,30 @@
+import string
 
+def main():
+    operational()
+    print()
+    lookUp()
+    print()
+    removal()
+    print()
+    update()
+    print()
+    aggression()
+    print()
+    count_letters()
+    
+def operational():
+    person = {"name":"",
+              "age" : None,
+              "location":"",
+              }
+    
+    person =add(person)
+    
+    for key, value in person.items():
+        print(f"{key} : {value}")
+    
+     
+ 
+if __name__ == "__main__":
+    main()
